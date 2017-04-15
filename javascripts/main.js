b79696253a37a05666b5a4806f574835d97d58b7
Chuang Yu <cyu9960@gmail.com>
2017-04-15 17:35:02 +0800

    tweak: 更改回复显示方式，在楼层中显示。

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,139 +3,125 @@
   */
 
 // Use print as console.log
-let print = console.log.bind(console);
+let print = console.log.bind(console)
 
 // The html
-const replyBoxHtmlTemplate = () => `
-    <li>{{replace}}</li>
-    `
+const replyBoxHtmlTemplate = () => `<p>{{replace}}</p>`
 
 // All replys in current page.
-let allDoms = () => document.querySelectorAll('.reply_content');
+let allDoms = () => document.querySelectorAll('.reply_content')
 
 // Does a reply @someone ?
 let isMetioningSomeOne = (content) => {
-    return content.indexOf("@") > -1
+  return content.indexOf("@") > -1
 }
 
 // Map util.
 let map = (target, fn) => {
-    if (Array.isArray(target)) {
-        return Array.prototype.map.call(target, fn);
-    } else {
-        let re = [],
-            val;
-        for (let key in target) {
-            val = target[key];
-            re.push(fn(val, key));
-        }
-        return re;
+  if (Array.isArray(target)) {
+    return Array.prototype.map.call(target, fn)
+  } else {
+    let re = [],
+      val
+    for (let key in target) {
+      val = target[key]
+      re.push(fn(val, key))
     }
+    return re
+  }
 }
 
 // Get text of a dom.
 let getText = (e) => {
-    let t = "";
-    e = e.childNodes || e;
-    for (let j = 0; j < e.length; j++) {
-        if (!e[j]) continue;
-        t += e[j].nodeType != 1 ?
-            e[j].nodeValue : getText(e[j].childNodes);
-    }
-    return t;
+  let t = ""
+  e = e.childNodes || e
+  for (let j = 0; j < e.length; j++) {
+    if (!e[j]) continue
+    t += e[j].nodeType != 1 ?
+      e[j].nodeValue : getText(e[j].childNodes)
+  }
+  return t
 }
 
 // Get all metioned users in a reply.
 let getMetioned = (content) => {
-    let o = content.match(/\@[^\s]*/g) || [];
-    return map(o, (val) => {
-        return val.replace(/\@/, '');
-    })
+  let o = content.match(/\@[^\s]*/g) || []
+  return map(o, (val) => {
+    return val.replace(/\@/, '')
+  })
 }
 
 // Insert html
 let setBoxHtml = (dom, users, level) => {
-    let li = replyBoxHtmlTemplate(),
-        html = '';
-    map(users, (user) => {
-        let contents = getUserContents(user, level);
-        map(contents, (content) => {
-            html += li.replace(/\{\{replace\}\}/g, `
+  let li = replyBoxHtmlTemplate(),
+    html = ''
+  map(users, (user) => {
+    let contents = getUserContents(user, level)
+    map(contents, (content) => {
+      html += li.replace(/\{\{replace\}\}/g, `
                 ${user}: ${content}
-            `);
-        })
+            `)
     })
-    dom.innerHTML = html;
+  })
+  dom.innerHTML = html
 }
 
 // 
 let setReplyBoxDom = (dom, users) => {
-    // Create a box.
-    let div = document.createElement('div');
-    div.innerHTML = replyBoxHtmlTemplate;
-    div.setAttribute('class', 'reply_box');
+  // Create a box.
+  let div = document.createElement('div')
+  div.innerHTML = replyBoxHtmlTemplate
+  div.setAttribute('class', 'reply_box')
 
-    let level = 0;
-    try {
-        level = parseInt(dom.parentElement.querySelector('.fr .no').textContent);
-    } catch (err) {
-        print(err);
-        level = 0
-    }
+  let level = 0
+  try {
+    level = parseInt(dom.parentElement.querySelector('.fr .no').textContent)
+  } catch (err) {
+    print(err)
+    level = 0
+  }
 
-    // Insert contents.
-    setBoxHtml(div, users, level);
+  // Insert contents.
+  setBoxHtml(div, users, level)
 
-    // Append to dom.
-    dom.appendChild(div);
-
-    // Bind events listeners.
-    dom.onmouseover = () => {
-        setTimeout(() => {
-            dom.lastChild.style.display = 'block';
-        }, 300)
-    }
-    dom.onmouseout = () => {
-        setTimeout(() => {
-            dom.lastChild.style.display = 'none';
-        }, 300)
-    }
+  // Prepend to dom.
+  dom.insertBefore(div, dom.firstChild)
 }
 
 //
 let checkDom = (dom) => {
-    let content = getText(dom);
-    if (content && isMetioningSomeOne(content)) {
-        dom.style.position = 'relative';
-        setReplyBoxDom(dom, getMetioned(content));
-    }
+  let content = getText(dom)
+  if (content && isMetioningSomeOne(content)) {
+    dom.style.position = 'relative'
+    setReplyBoxDom(dom, getMetioned(content))
+  }
 }
 
 // Get replys of specified user.
 let getUserContents = (user, level) => {
-    let re = [];
-    for (let i = 0, l = allContents.length - 1; i < l; i++) {
-        if (allContents[i].user === user && i < level) {
-            re.push(allContents[i].content);
-        }
+  let re = []
+  for (let i = 0, l = allContents.length - 1; i < l; i++) {
+    if (allContents[i].user === user && i < level) {
+      re.push(allContents[i].content)
     }
-    return re
+  }
+  return re
 }
 
 // ============================================
 let allContents = map(allDoms(), (dom) => {
-    if (!dom || !dom.previousElementSibling) return {}
-    let user = '';
-    try {
-        user = dom.parentElement.querySelector('strong a').text;
-    } catch (err) {
-        print(err, dom);
-        user = '';
-    }
-    return {
-        user: user,
-        content: getText(dom)
-    }
+  if (!dom || !dom.previousElementSibling) return {}
+  let user = ''
+  try {
+    user = dom.parentElement.querySelector('strong a').text
+  } catch (err) {
+    print(err, dom)
+    user = ''
+  }
+  return {
+    user: user,
+    content: getText(dom)
+  }
 })
 
-map(allDoms(), checkDom);
+map(allDoms(), checkDom)
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # v2ex-reply
 
-快速显式回复的楼层内容，方便浏览。
+在楼层中显示回复者的内容。
 
 ![截图](./images/capture.png)
+
+## Usage
+
+`git clone https://github.com/cyyyu/v2ex-reply.git`
+
+在chrome插件页面中加载该插件即可。
