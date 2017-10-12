be9faeb9d4c3f4bd8308af95306dfb64cb9f6e6f
cyyyu <cyu9960@gmail.com>
2017-10-12 18:26:12 +0800

    rename module

--- src/getUsers.ts
+++ src/getUsers.ts
@@ -8,7 +8,7 @@ const getUserContents = (user: string, level: number): Array<string> => {
   const re = []
   map(allContents, (content) => {
     if (content.user === user && content.level < level) {
-      re.push(content.content)
+      re.push(content)
     }
   })
   return re
