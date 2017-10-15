2590a19eb55431d0d35e88ae2d9dd983688411c0
cyyyu <cyu9960@gmail.com>
2017-10-15 12:31:32 +0800

    tweak ui

--- src/setHtml.ts
+++ src/setHtml.ts
@@ -1,18 +1,19 @@
 // Insert html
 
-import template from './template'
 import map from './helpers/map'
 import getUsers from './getUsers'
 
 const setHtml = (dom: HTMLElement, users: Array<string>, level?: number) => {
-  const li = template()
   let html = ''
   map(users, (user) => {
     const users = getUsers(user, level)
     map(users, user => {
       const content = user.content
-      const head = `<img style='height: 14px;vertical-align: middle;' src='${user.avatar}' />`
-      html += li.replace(/\{\{replace\}\}/g, `${head} ${user.user}: ${content}`)
+      const head = `<img style='height: 24px;vertical-align: middle;' src='${user.avatar}' />`
+      html += `
+        <p>
+          ${head} <a href='https://www.v2ex.com/member/${user.user}'>${user.user}</a>: ${content}
+        </p>`
     })
   })
   if (!html) dom.setAttribute('style', 'background: #cecece; color: white;')
--- src/template.ts
+++
@@ -1,5 +0,0 @@
-// The html template which is used for reply-box content
-
-const replyBoxHtmlTemplate = () => `<p>{{replace}}</p>`
-
-export default replyBoxHtmlTemplate
