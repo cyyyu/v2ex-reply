83bd8716e40720069da44cc65b9d0189b15b3fe1
Chuang Yu <cyu9960@gmail.com>
2023-05-23 12:20:43 +0800

    1. Highlight mentioned user if it's the author 2. Set max height

--- src/setHtml.ts
+++ src/setHtml.ts
@@ -3,16 +3,20 @@
 import map from './helpers/map'
 import getUsers from './getUsers'
 
-const setHtml = (dom: HTMLElement, users: Array<string>, level?: number) => {
+const setHtml = (dom: HTMLElement, users: Array<string>, level?: number, currentUser?: string) => {
   let html = ''
   map(users, (user) => {
     const users = getUsers(user, level)
     map(users, user => {
-      const content = user.content
+      let content = user.content
+
+      // Replace "@currentUser" in the content as a tag
+      content = content.replace(new RegExp('@' + currentUser, 'g'), `<span style='font-weight: bold; color: black;' >@${currentUser}</span>`)
+
       const head = `<img style='height: 24px;vertical-align: middle;' src='${user.avatar}' />`
       html += `
         <p>
-          ${head} <a href='https://www.v2ex.com/member/${user.user}'>${user.user}</a>: ${content}
+          ${head} <a href='/member/${user.user}'>${user.user}</a>: ${content}
         </p>`
     })
   })
--- src/setReplyBox.ts
+++ src/setReplyBox.ts
@@ -5,11 +5,17 @@ const setReplyBoxDom = (dom: HTMLElement, users: Array<string>) => {
   // Create a box.
   let div = document.createElement('div')
   div.setAttribute('class', 'reply_box')
+  div.style.maxHeight = '300px'
+  div.style.overflow = 'auto';
+  (div.style as any).overscrollBehavior = 'contain'
 
   const level = parseInt(dom.parentElement.querySelector('.fr .no').textContent)
 
+  // Find current user
+  const currentUser = dom.parentElement.querySelector("strong a").textContent
+
   // Insert contents.
-  setHtml(div, users, level)
+  setHtml(div, users, level, currentUser)
 
   // Prepend to dom.
   dom.insertBefore(div, dom.firstChild)
